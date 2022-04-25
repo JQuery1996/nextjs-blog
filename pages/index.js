62bca850802043b39f65c0e0f61606c7a5dad656
JQuery1996 <jquery.1996@gmail.com>
2022-04-25 07:00:15 +0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from "next/Head";
+import Head from "next/head";
 import Link from "next/link";
 import { Layout, siteTitle, Date } from "../components";
 import utilStyles from "../styles/utils.module.css";
